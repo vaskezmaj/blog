d428055a426271d9b32ff8868f60e0c4a8e99dfb
Vaskezmaj <aleksandar.vasic018@gmail.com>
2022-03-28 22:22:14 +0200

    komit sa postgresom

--- Sajt/settings.py
+++ Sajt/settings.py
@@ -75,10 +75,15 @@ WSGI_APPLICATION = 'Sajt.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'mydb',
+        'USER': 'aca',
+        'PASSWORD': 'kosovo99',
+        'HOST': 'localhost',
+        'PORT': '',
     }
 }
+
 
 # Password validation
 # https://docs.djangoproject.com/en/4.0/ref/settings/#auth-password-validators
